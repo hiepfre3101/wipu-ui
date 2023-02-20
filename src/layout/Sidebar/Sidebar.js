533df17c71ad772ddf31b2ef62456c02b301dc93
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2023-02-20 21:19:16 +0700

    Fix after deploy #3

--- src/layout/Sidebar/Sidebar.js
+++ src/layout/Sidebar/Sidebar.js
@@ -16,23 +16,25 @@ const FavoriteAnime = withContent(FavoriteList, selectRequestTopAnime, false, []
 function Sidebar() {
    return (
       <div className={cx('wrapper')}>
-         <div className={cx('nav-aside')}>
-            <Button roundM leftIcon={<HomeIcon />} className={cx('nav-btn')} primary nonBg to={'/'}>
-               Home
-            </Button>
-            <Button roundM leftIcon={<StonkeIcon />} className={cx('nav-btn')} primary nonBg to={'/popular'}>
-               Popular
-               <svg width={24} height={24} className={cx('animate')}>
-                  <circle cx={12} cy={12} r={12} fill={'#99f6e4'}></circle>
-               </svg>
-            </Button>
-            <Button roundM leftIcon={<GenreIcon />} className={cx('nav-btn')} primary nonBg to={'/genre'}>
-               Genre
-            </Button>
-         </div>
-         <div className={cx('favorite-block')}>
-            <FavoriteAnime />
-         </div>
+      <div className={cx('top-block')}>
+            <div className={cx('nav-aside')}>
+               <Button roundM leftIcon={<HomeIcon />} className={cx('nav-btn')} primary nonBg to={'/'}>
+                  Home
+               </Button>
+               <Button roundM leftIcon={<StonkeIcon />} className={cx('nav-btn')} primary nonBg to={'/popular'}>
+                  Popular
+                  <svg width={24} height={24} className={cx('animate')}>
+                     <circle cx={12} cy={12} r={12} fill={'#99f6e4'}></circle>
+                  </svg>
+               </Button>
+               <Button roundM leftIcon={<GenreIcon />} className={cx('nav-btn')} primary nonBg to={'/genre'}>
+                  Genre
+               </Button>
+            </div>
+            <div className={cx('favorite-block')}>
+               <FavoriteAnime />
+            </div>
+      </div>
          <footer className={cx('footer')}>
             <p className={cx('footer-title')}>EE Watch Copyright © 2022</p>
          </footer>
